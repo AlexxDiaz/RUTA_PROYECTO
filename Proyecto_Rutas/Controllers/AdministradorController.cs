de542aee3a73f2f5b29091aaa3570b3c7f4093ff
AlexxDiaz <33430480+AlexxDiaz@users.noreply.github.com>
2017-11-09 13:55:42 -0600

    subiendo
    proyecto

---
+++ Proyecto_Rutas/Controllers/AdministradorController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto_Rutas.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        // GET: Administrador
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Admi()
+        {
+            return View();
+        }
+    }
+}
---
+++ Proyecto_Rutas/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto_Rutas.Startup))]
+namespace Proyecto_Rutas
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
